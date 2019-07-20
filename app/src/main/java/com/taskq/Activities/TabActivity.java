36bbe18d5142140e27b7fa68796c82f0447ad164
zerowolf <zerowolf@zerowolf.com>
2019-07-20 15:37:37 +0530

    Placeholder before adding fragments to build.

--- app/src/main/java/com/taskq/Activities/TabActivity.java
+++ app/src/main/java/com/taskq/Activities/TabActivity.java
@@ -1,17 +1,40 @@
 package com.taskq.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
 import com.taskq.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TabActivity extends AppCompatActivity {
+
+    private Toolbar tab_Toolbar;
+    private TabLayout tab_TabLayout;
+    private ViewPager tab_ViewPager;
+    private int[] tabIcons = {
+            R.drawable.ic_tab_all_light,
+            R.drawable.ic_tab_what_light,
+            R.drawable.ic_tab_when_light,
+            R.drawable.ic_tab_who_light
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab);
+
+        tab_Toolbar = findViewById(R.id.tab_toolbar);
+        setSupportActionBar(tab_Toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        tab_ViewPager = findViewById(R.id.tab_ViewPager);
+
     }
 
 //    @Override
@@ -42,5 +65,6 @@ public class TabActivity extends AppCompatActivity {
 //    protected void onDestroy() {
 //        super.onDestroy();
 //    }
+
 
 }
