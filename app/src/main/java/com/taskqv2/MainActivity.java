5ee1fbec710e71da4e36733eec16fe1b61d32842
zerowolf <zerowolf@zerowolf.com>
2019-07-20 12:55:41 +0530

    Splash screen added.

--- app/src/main/java/com/taskqv2/MainActivity.java
+++ app/src/main/java/com/taskqv2/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        setTheme(R.style.AppTheme_Main);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
