bf90538fbf9be595edf9b1d6877de64bc4a16b47
zerowolf <zerowolf@zerowolf.com>
2019-11-01 16:19:57 +0530

    Who functionality ported

--- app/src/main/java/com/taskq/Fragments/WhoFragment.java
+++ app/src/main/java/com/taskq/Fragments/WhoFragment.java
@@ -1,6 +1,7 @@
 package com.taskq.Fragments;
 
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 
@@ -10,10 +11,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.taskq.Activities.taskQEntryActivity;
+import com.taskq.CustomClasses.customExpandableListAdapter;
+import com.taskq.CustomClasses.taskQGlobal;
 import com.taskq.DataBase.dBaseArchitecture;
+import com.taskq.DataBase.dBaseArchitecture_Who;
 import com.taskq.DataBase.dBaseManager;
 import com.taskq.DataBase.dBaseManager_Who;
 import com.taskq.R;
@@ -21,7 +28,9 @@ import com.taskq.Settings.taskQSettings;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -35,6 +44,15 @@ public class WhoFragment extends Fragment {
     private ListView listView;
     private taskQSettings Settings;
     private int maxProgressBar;
+    private ExpandableListView expandableListView;
+
+    //Expandable List Vars
+    private HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
+    private HashMap<String, List<String>> expandableListIDs    = new HashMap<String, List<String>>();
+    private HashMap<String, List<String>> expandableListTagCnt = new HashMap<String, List<String>>();
+    private List<String>                  expandableListTags;
+    private ExpandableListAdapter expandableListAdapter;
+    public static String                  strSeparator;
 
     public WhoFragment() {
         // Required empty public constructor
@@ -48,8 +66,8 @@ public class WhoFragment extends Fragment {
         dbManager.open();
         dbManager_Who = new dBaseManager_Who(getActivity());
         dbManager_Who.open();
-        listView = view.findViewById(R.id.ListView_Frag_Who);
         Settings = new taskQSettings(getActivity().getApplicationContext());
+        expandableListView = view.findViewById(R.id.ListView_Frag_Who_Expandable);
         return view;
     }
 
@@ -64,15 +82,16 @@ public class WhoFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
+        strSeparator = getString(R.string.strSeparator);
         ArrayList strRawTags = new ArrayList();
         ArrayList strRecurrringTagsBuffer = new ArrayList();
         ArrayList strRecurrringTags = new ArrayList();
         Set<String> hsRecurringTags = new HashSet<>();
         ArrayList strTags = new ArrayList();
-
-
-        Cursor cursorWho = dbManager_Who.fetch();
         Cursor cursor;
+        Cursor cursorWho = dbManager_Who.fetch();
+        Cursor cursorWhoTaskList;
+
         if(Settings.getSwitchShowCompleted() == true){
             cursor = dbManager.fetch();}
         else{
@@ -99,7 +118,7 @@ public class WhoFragment extends Fragment {
         //
         for (int x=0; x<strRawTags.size(); x++){
             //This will give us a array list of arrays
-            strRecurrringTagsBuffer.add(strRawTags.get(x).toString().split(","));
+            strRecurrringTagsBuffer.add(strRawTags.get(x).toString().split(strSeparator));
         }
         for (int x=0; x<strRecurrringTagsBuffer.size(); x++) {
             //Local Buffer to dereference Array List
@@ -137,7 +156,64 @@ public class WhoFragment extends Fragment {
         }
         maxProgressBar = maxProgressBar + 1;
 
+        //Step 6 - expandableListDetail - Must be a hash of Keys = Tags and List as List<String> of Task COL_TASK
+        cursorWho = dbManager_Who.fetch();
+        for(cursorWho.moveToFirst() ; !cursorWho.isAfterLast() ; cursorWho.moveToNext()){
+            List<String> strLstTaskNameBuffer = new ArrayList<String>();
+            List<String> strLstTaskIDBuffer   = new ArrayList<String>();
+            List<String> strLstTaskCntBuffer  = new ArrayList<String>();
 
+            //strCurrentTag now has the Tag Name
+            String strCurrentTag = cursorWho.getString(cursorWho.getColumnIndex(dBaseArchitecture_Who.COL_WHO));
+            //Get all Tasks that have the set tag
+            if(Settings.getSwitchShowCompleted() == true){
+                cursorWhoTaskList =  dbManager.fetchEntryByName(strCurrentTag);
+            }
+            else{
+                cursorWhoTaskList =  dbManager.fetchEntryByName_NoCompleted(strCurrentTag);
+            }
+
+            //Step 7 - Make the HashMap of the Task Names against Tag
+            //Parse the list of Tasks to get make the task Description List
+            for(cursorWhoTaskList.moveToFirst() ; !cursorWhoTaskList.isAfterLast() ; cursorWhoTaskList.moveToNext()){
+                String strCurrentWhatTask = cursorWhoTaskList.getString(cursorWhoTaskList.getColumnIndex(dBaseArchitecture.COL_TASK));
+                strLstTaskNameBuffer.add(strCurrentWhatTask);
+            }
+            //Now we have the Tag in strCurrentTag and all Tasks with names in strLstTaskNameBuffer. Add to HashMap as Key and Value
+            expandableListDetail.put(strCurrentTag, strLstTaskNameBuffer);
+
+            //Step 8 - Make the HashMap of the Task IDs against Tag
+            //Parse the list of Tasks to get make the task ID List
+            for(cursorWhoTaskList.moveToFirst() ; !cursorWhoTaskList.isAfterLast() ; cursorWhoTaskList.moveToNext()){
+                String strCurrentWhatTaskID = cursorWhoTaskList.getString(cursorWhoTaskList.getColumnIndex(dBaseArchitecture._ID));
+                strLstTaskIDBuffer.add(strCurrentWhatTaskID);
+            }
+            //Now we have the Tag in strCurrentTag and all Tasks with names in strLstTaskNameBuffer. Add to HashMap as Key and Value
+            expandableListIDs.put(strCurrentTag, strLstTaskIDBuffer);
+
+            //Step 9 - Make the Hashmap of the Tags againt Tag Count. This is a simple one element value against the Tag Key.
+            strLstTaskCntBuffer.add(cursorWho.getString(cursorWho.getColumnIndex(dBaseArchitecture_Who.COL_WHO_COUNT)));
+            expandableListTagCnt.put(strCurrentTag, strLstTaskCntBuffer);
+        }
+
+        //Step 10 - Call the customExpandableListAdapter to get the list View
+        expandableListTags = new ArrayList<String>(expandableListDetail.keySet());
+        expandableListAdapter = new customExpandableListAdapter(getActivity(), expandableListTags, expandableListDetail, expandableListIDs, expandableListTagCnt, maxProgressBar);
+        expandableListView.setAdapter(expandableListAdapter);
+
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                int idModify = Integer.parseInt(expandableListIDs.get(expandableListTags.get(groupPosition)).get( childPosition));
+                if(true == (((taskQGlobal) getActivity().getApplication()).bSetUserEntryModify(idModify))){
+                    Intent modifyIntent = new Intent(getActivity().getApplicationContext(), taskQEntryActivity.class);
+                    startActivity(modifyIntent);
+                }
+                return false;
+            }
+        });
     }
 
 
