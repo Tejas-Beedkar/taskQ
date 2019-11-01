06a718a1f3ff900c96074c873d1cac59e6039929
zerowolf <zerowolf@zerowolf.com>
2019-11-01 20:00:39 +0530

    Basic vertical bar implemented

--- app/src/main/java/com/taskq/Fragments/HomeFragment.java
+++ app/src/main/java/com/taskq/Fragments/HomeFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.taskq.R;
 
@@ -16,6 +17,7 @@ import com.taskq.R;
  */
 public class HomeFragment extends Fragment {
 
+    private View view;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -26,7 +28,19 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        ProgressBar bar_1 = view.findViewById(R.id.HomeFrag_pbar_1);
+
+        bar_1.setMax(100);
+        bar_1.setProgress(1);
     }
 
 }
