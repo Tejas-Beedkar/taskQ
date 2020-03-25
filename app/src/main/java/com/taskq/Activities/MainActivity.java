19da59119cda8708b342eaae582bf756323c6964
zerowolf <zerowolf@zerowolf.com>
2020-03-25 13:40:22 +0530

    Dummy Modifications

--- app/src/main/java/com/taskq/Activities/MainActivity.java
+++ app/src/main/java/com/taskq/Activities/MainActivity.java
@@ -14,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //Dummy Modification
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
